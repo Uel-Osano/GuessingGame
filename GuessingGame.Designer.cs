55f4fc8c87eca347952bbcc57b07c52b7ce02d54
Uel <Uel@LAPTOP-NANQ0C65>
2023-03-25 14:20:45 +0200

    Add project files.

---
+++ GuessingGame.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace GuessingGame
+{
+    partial class GuessingGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblmsg = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(528, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How quickly can you guess the random number?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "What number (0 - 100)?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(133, 145);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Guess Count:";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(252, 101);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(128, 26);
+            this.txtGuess.TabIndex = 3;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnGuess.Location = new System.Drawing.Point(166, 215);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(115, 43);
+            this.btnGuess.TabIndex = 5;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnReset.Location = new System.Drawing.Point(338, 248);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(83, 40);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblmsg
+            // 
+            this.lblmsg.AutoSize = true;
+            this.lblmsg.Location = new System.Drawing.Point(172, 179);
+            this.lblmsg.Name = "lblmsg";
+            this.lblmsg.Size = new System.Drawing.Size(0, 20);
+            this.lblmsg.TabIndex = 7;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(249, 149);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 20);
+            this.lblAnswer.TabIndex = 8;
+            // 
+            // GuessingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(437, 300);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblmsg);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "GuessingGame";
+            this.Text = "Guessing Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblmsg;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ GuessingGame.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class GuessingGame : Form
+    {
+        private int guess;
+        private int target;
+        private int countofGuesses;
+        private Random ranValue;
+
+        public GuessingGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ranValue = new Random();
+            target = ranValue.Next(0, 101);
+            countofGuesses = 0;
+
+
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+
+            if (int.TryParse(txtGuess.Text, out guess) == false)
+            {
+                lblmsg.Text = "Value must be numberic";
+            }
+            else
+            {
+                lblmsg.Text = "";
+                lblAnswer.Text = (++countofGuesses).ToString();
+
+                if (guess == target)
+                {
+                    BackColor = Color.Purple;
+                    MessageBox.Show(" Your guess is " + guess + " You got it... You won!");
+                    lblmsg.Text = "Reset the game to start again";
+
+
+
+                }
+                else
+                {
+                    txtGuess.Clear();
+                    txtGuess.Focus();
+                    while (target != guess)
+                     
+                        {
+                            if (guess > target)
+                            {
+
+                                {
+                                if ( target %2==0)
+                                {
+                                    MessageBox.Show(guess + " is too high" + " The answer is Even");
+                                    BackColor = Color.Red;
+                                    break;
+                                }
+                                else
+                                {
+                                    MessageBox.Show(guess + " is too high" + " The answer is Odd");
+                                    BackColor = Color.Red;
+                                    break;
+                                }
+                               
+                                   
+                                }
+
+                            }
+
+
+                            else
+                            {
+                            if (target % 2 == 0)
+                            {
+                                MessageBox.Show(guess + " is too low" + " The answer is Even");
+                                BackColor = Color.Green;
+                                break;
+                            }
+                            else
+                            {
+                                MessageBox.Show(guess + " is too low" + " The answer is Odd");
+                                BackColor = Color.Green;
+                                break;
+                            }
+
+
+                            }
+
+
+
+                        }
+
+
+                }
+            }
+
+
+
+
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            target = ranValue.Next(0, 101);
+            countofGuesses = 0;
+            BackColor = Color.Purple;
+            txtGuess.Clear();
+            lblAnswer.Text = "";
+            lblmsg.Text = "";
+            txtGuess.Focus();
+        }
+    }
+}
